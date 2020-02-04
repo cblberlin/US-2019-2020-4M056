a939adc5373f8ef0ec326489bd1b0cb8eb43f33c
cblberlin <bailin.cai@hotmail.fr>
2020-02-04 10:46:50 +0100

    Update matrice.cpp

--- TP2/matrice.cpp
+++ TP2/matrice.cpp
@@ -28,7 +28,7 @@ MatrixDouble puissance(const MatrixDouble & M, int n){
 			return M*N;
 		}
 	}
-// 2.3	
+// 2.3
 MatrixDouble puissanceB(const MatrixDouble M, int n){
 		if(n==0){
 			return MatrixDouble::Identity(M.rows(),M.cols());
@@ -80,7 +80,7 @@ SparseDouble puissance_sparse(const SparseDouble & M, int n){
 
 // 2.8
 MatrixDouble crochet_lie(const MatrixDouble & A, const MatrixDouble & B){
-	return A*B - B*A;                                                                                                                                                                                                                                                                                      
+	return A*B - B*A;
 }
 
 // 2.9
@@ -110,40 +110,40 @@ int main(){
 		 0., 0., 1.;
 	cout << "A = \n\n" << A << ".\n\n";
 	cout << "A^10000 = \n\n" << puissance(A, n) << ".\n\n";
-	
+
 	// 2.3
 	chrono::time_point<chrono::system_clock> start, end;
 	start = chrono::system_clock::now();
 	puissance(A,n);
 	end = chrono::system_clock::now();
-	
+
 	chrono::time_point<chrono::system_clock> start2, end2;
 	start2 = chrono::system_clock::now();
 	puissanceB(A,n);
 	end2 = chrono::system_clock::now();
-	
+
 	auto executed_time1 = chrono::duration<double>(end - start).count();
 	auto executed_time2 = chrono::duration<double>(end2 - start2).count();
-	
+
 	cout << "Avec & on a utilisé " << executed_time1 << " secondes" << "\n\n";
 	cout << "Sans & on a utilisé " << executed_time2 << " secondes" << "\n\n";
-	
+
 	ifstream o("matrice.dat");
 	MatrixDouble M(30,30);
 	for (int i = 0; i < 30; ++i){
 		for (int j = 0; j < 30; ++j){
 			o >> M(i,j);
-		}		
+		}
 	}
 	cout << "M est: \n" << M << "\n\n";
-	
+
 	// 2.5
 	auto t1 = chrono::system_clock::now();
 	puissance2(M,n);
 	auto t2 = chrono::system_clock::now();
 	chrono::duration<double> diff = t2 - t1;
 	cout << "Pour calculer M puissance 10000 en utilisant puissance2 ça prend: \n" << endl << diff.count() << "s."<<endl;
-	
+
 	//2.6
 	SparseDouble M2(M.rows(),M.cols());
 	double c;
@@ -155,19 +155,19 @@ int main(){
 			}
 		}
 	}
-	
+
 	auto t3 = chrono::system_clock::now();
 	puissance(M,n);
 	auto t4 = chrono::system_clock::now();
 	chrono::duration<double> diff2 = t4 - t3;
 	cout << "Pour calculer M puissance 10000 en utilisant puissance ça prend: \n" << endl << diff2.count() << "s."<<endl;
-	
+
 	auto t5 = chrono::system_clock::now();
 	puissance_sparse(M2, n);
 	auto t6 = chrono::system_clock::now();
 	chrono::duration<double> diff3 = t6 - t5;
 	cout << "Pour calculer M puissance 10000 en utilisant puissance_sparse ça prend: \n" << endl << diff3.count() << "s."<<endl;
-	
+
 	//2.10
 	//Initialisation
 
@@ -197,17 +197,26 @@ int main(){
 	cout << "moyenne de M_A est: " << s_x/double(nb_simulation) <<endl;
 	cout << "moyenne de M_B est: " << s_y/double(nb_simulation) <<endl;
 
+	//2.11
+	//créer une histogramme
+	const int M = 20;
+	vector<double> hist(M,0);
+	//loi sur le diag et hor diag
+	normal_distribution<double> Loi1(0,1);
+	normal_distribution<double> Loi2(0,2);
+	//GOE, calculer les valeurs propres
+	MatrixDouble GOE(150, 150);
+	for(int i = 0, i < 150; ++i){
+		GOE(i,i) = Loi1(G);
+		for(int j = i + 1, j < 150; ++j){
+			GOE(i,j) = Loi2(G);
+			GOE(j,i) = GOE(i,j);
+		}
+	}
+	EigenSolver<MatrixDouble> Solver(M);
+
+	
+
+
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
